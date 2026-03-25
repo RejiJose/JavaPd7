d7ba0cc9106b35b7f74cf92df1668fd04a6f4291
Reji Joseph <rjoseph82@schools.nyc.gov>
2026-03-25 15:08:07 +0000

    hh

--- JAVA_45-50/Java-46-Discussion-soln/Main.java
+++ JAVA_45-50/Java-46-Discussion-soln/Main.java
@@ -24,42 +24,42 @@ public class Main {
 
   void init() throws IOException {
    
-    // Create a port - this is our Gateway
+    // 1. Create a port - this is our Gateway
     int port = 8000;
       
-    // Create the HTTPserver object
+    // 2. Create the HTTP Server object
     HttpServer server = HttpServer.create(new InetSocketAddress(port),0);
 
-    // Start the server      
+    // 3. Start the server      
     server.start();
     System.out.println("Server is listening on port " + port);     
 
     
-    // Create a Route Handler to respond to the request
+    // 4. Create a Route Handler to respond to the request
     // this is our default route
     server.createContext("/", new RouteHandler("Hi there. This is my default route...") );
     
-    // 1) create a route named 'myPage1'
+    // 4a) create a route named 'myPage1'
     String html = "<html><body><marquee><h1>Here's my 1ST Webpage !!</h1></marquee>";
-	html +="<h3>Info that scrolls is a Marquee element</h3></body></html>"; 
+	  html +="<h3>Info that scrolls is a Marquee element</h3></body></html>"; 
     server.createContext("/myPage1", new RouteHandler(html) );
     
-    // 2) create a route named 'myPage2'
+    // 4b) create a route named 'myPage2'
     html = "<html><body>";
-	html +=		"<marquee>";
-	html +=			"<h1>This is my 2ND Webpage.</h1><br>";
-	html +=			"<img src='https://cdn.pixabay.com/photo/2019/02/19/19/45/thumbs-up-4007573_960_720.png'>"; 
-	html +=			"<h3>This page has a picture</h3>";
-	html +=		"</marquee>";
-	html +="</body></html>";	
+    html +=		"<marquee>";
+    html +=			"<h1>This is my 2ND Webpage.</h1><br>";
+    html +=			"<img src='https://cdn.pixabay.com/photo/2019/02/19/19/45/thumbs-up-4007573_960_720.png'>"; 
+    html +=			"<h3>This page has a picture</h3>";
+    html +=		"</marquee>";
+    html +="</body></html>";	
     server.createContext("/myPage2", new RouteHandler(html) );
 	// Note: only use external image sources
     
-     // 3) create a route named 'myPage3'
+     // 4c) create a route named 'myPage3'
     html = Input.readFile("index1.html");// reads file and returns a string
     server.createContext("/myPage3", new RouteHandler(html) );
 
-    // 4) create a route named 'myPage4'
+    // 4d) create a route named 'myPage4'
     html = Input.readFile("index2.html");
     server.createContext("/myPage4", new RouteHandler(html) );  
       
