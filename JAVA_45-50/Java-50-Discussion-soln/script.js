a90ad99cb9f60b7ac5e5f89bb138b23453471b6f
Reji Joseph <rjoseph82@schools.nyc.gov>
2026-04-13 13:02:07 +0000

    dd

--- JAVA_45-50/Java-50-Discussion-soln/script.js
+++ JAVA_45-50/Java-50-Discussion-soln/script.js
@@ -1,15 +1,20 @@
-let data;
+let data, info;
 
-function init(){
-  $.ajaxSetup({async: false});
-  
-  let link = "http://localhost:8400";
+async function init(){
+  // JSON data retrieval process  
+  let link = "https://ubiquitous-guacamole-wvrrvgg65qpf9j59-8500.app.github.dev"; 
+  // The URL above must be running in a separate broswer tab. (Alternate use: http://localhost:8500 )
   let route= "/customers";
-  let cust_array = $.getJSON(link+route).responseJSON;
+
+  info = await fetch(link+route);
+  data = await info.json();
+
+  let cust_array = data;
+  console.log(customers); // confirm data retrieval
 
   generateCards(cust_array);
-
 }
+
 
 function generateCards(customers){
   let output = document.getElementById("output");
